7d7889e7b6640b199ca2aa90d6612eb48ebbab59
rohan26ir <rohan26ir@gmail.com>
2026-07-22 12:24:19 +0600

    add vercel.json

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-// server.js
-
 require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
 const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB then start server
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`\n🚀  FinTracker API running on http://localhost:${PORT}`);
-    console.log(`📦  Environment: ${process.env.NODE_ENV || "development"}\n`);
+    console.log(`🚀 FinTracker API running on http://localhost:${PORT}`);
   });
 });
+
+// Important for Vercel
+module.exports = app;
